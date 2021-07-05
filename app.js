1fa30339f7516200fd7c9c0a1fe5f96a25983f49
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-07-04 22:29:00 -0400

    Completed.  Last commit for today

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const ul = document.querySelector("#user-list");
+  const leftDiv = document.querySelector('.left-div');
+  const rightDiv = document.querySelector('.right-div');
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((e) => e.json())
     .then((apidata) => {
       apidata.forEach((element) => {
         const li = document.createElement("li");
-        li.innerText = element.name;
+        button = document.createElement("button");
+        button.innerText = element.name;
+        button.addEventListener('click', (event) => {
+          rightDiv.innerHTML = '';
+          rightDiv.append(JSON.stringify(element));
+        });
+        li.append(button);
         ul.append(li);
+        leftDiv.append(ul);
       });
     })
     .catch((e) => console.log("ERR", e));
